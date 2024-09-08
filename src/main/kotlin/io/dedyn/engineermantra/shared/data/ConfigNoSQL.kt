0bb33b0c351430ccb0a84eb805c9140836c6dc47
MaxDistructo <MaxDistructo@users.noreply.github.com>
2024-09-07 20:00:24 -0500

    Start making NoSQL config

---
+++ src/main/kotlin/io/dedyn/engineermantra/shared/data/ConfigNoSQL.kt
@@ -0,0 +1,31 @@
+package io.dedyn.engineermantra.shared.data
+
+import com.datastax.oss.driver.api.core.CqlIdentifier
+import com.datastax.oss.driver.api.core.CqlSession
+import java.net.InetSocketAddress
+
+object ConfigNoSQL: ConfigFileInterface {
+
+    var session: CqlSession? = null
+
+    override fun load() {
+        session = CqlSession.builder()
+            .addContactPoint(InetSocketAddress("cassandra", 9042))
+            .withLocalDatacenter("datacenter")
+            .withKeyspace(CqlIdentifier.fromCql("botchelper"))
+            .build()
+    }
+
+    override fun save() {
+        session?.close()
+    }
+
+    override fun get(token: String): String? {
+        session?.execute("SELECT $token FROM thanks")
+        
+    }
+
+    override fun set(token: String, value: String) {
+        TODO("Not yet implemented")
+    }
+}
